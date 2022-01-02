ac12ee45ba82488fcdf1116907fa299021fb3681
Tomas Kislan <kislan.tomas@gmail.com>
2022-01-02 11:57:45 +0100

    Update dependencies, finalize CI build

---
+++ scripts/download_models.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -e
+
+SCRIPT_RELATIVE_DIR=$(dirname "${0}")
+ROOT_DIR=$(cd "${SCRIPT_RELATIVE_DIR}/.."; pwd -P)
+
+MODELS="ssd_mobilenet_v2_coco_2018_03_29 ssdlite_mobilenet_v2_coco_2018_05_09"
+
+opencv_tmpdir="$(mktemp -d)"
+mkdir -p "${opencv_tmpdir}"
+git clone --depth 1 https://github.com/opencv/opencv "${opencv_tmpdir}"
+
+for model in ${MODELS}; do
+    model_dir="${ROOT_DIR}/data/models/${model}"
+
+    if test -d "${model_dir}"; then
+        echo "Model ${model} already exists"
+        continue
+    fi
+
+    tmpdir="$(mktemp -d)"
+    mkdir -p "${tmpdir}"
+
+    pushd "${tmpdir}"
+
+    model_url="http://download.tensorflow.org/models/object_detection/${model}.tar.gz"
+    echo "${model_url}"
+    wget "${model_url}"
+
+    tar -xf "${model}.tar.gz"
+
+    PYTHONPATH="${opencv_tmpdir}/samples/dnn" \
+        python "${opencv_tmpdir}/samples/dnn/tf_text_graph_ssd.py" \
+            --input "${model}/frozen_inference_graph.pb" \
+            --config "${model}/pipeline.config" \
+            --output "${model}/frozen_inference_graph.pbtxt"
+    
+    mkdir -p "${model_dir}"
+    cp -v "${model}/frozen_inference_graph.pb" \
+        "${model}/pipeline.config" \
+        "${model}/frozen_inference_graph.pbtxt" \
+        "${model_dir}/"
+
+    popd
+
+    rm -rv "${tmpdir}"
+done
+
+rm -r "${opencv_tmpdir}"
